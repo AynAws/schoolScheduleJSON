70a2bac010137089fbd07144d10b5a463d7ee55c
aynaws <grimcreeperyt@gmail.com>
2024-10-30 10:35:51 -0400

    changed code to use filter... makes it less hard-coded

--- script.js
+++ script.js
@@ -8,24 +8,25 @@ $.ajax({
         const schedule = response.schedule;
         const tableBody = $("#schedule-table tbody");
         let build = [];
+        const getByPeriod = (period) => schedule.filter(item => item.period === period)[0];
         if (letter !== "G") {
           for (let i = 0; i < 3; i++) {
             let rotatedIndex = (i + letterIndex) % 4; // keeps numbers wrapping around 1-4
-            build.push(schedule[rotatedIndex + 1]);
+            build.push(getByPeriod(rotatedIndex + 1));
           }
         } else {
-          build = [schedule[3], schedule[4], schedule[7]]
+          build = [getByPeriod(3), getByPeriod(4), getByPeriod(7)]
         }
-        build.push(schedule[0]); // Always add schedule[0] (lunch) in the middle
+        build.push(getByPeriod(0)); // Always add schedule[0] (lunch) in the middle
         if (letter !== "G") {
           for (let ii = 0; ii < 2; ii++) {
             let latterRotatedIndex = (ii + letterIndex) % 3; // Rotates through [0, 1, 2]
             let periodIndex = [5, 6, 7][latterRotatedIndex]; // Map indices to periods [5, 6, 7]
-            build.push(schedule[periodIndex]);
+            build.push(getByPeriod(periodIndex));
           }        
         } else {
-          build.push(schedule[5]);
-          build.push(schedule[6]);
+          build.push(getByPeriod(5));
+          build.push(getByPeriod(6));
         }
         tableBody.find(".remove").remove();
         // Loop through the schedule array and create table rows
